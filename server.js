1ac9dc7fd228b6ff5c8eb2a0ccf7b73eeccdb169
unknown <y.litovinskyi@gmail.com>
2021-12-25 23:19:23 +0200

    better handle errors

--- server.js
+++ server.js
@@ -25,23 +25,27 @@ const sleep = (timeout) => {
 question.ask(async function(answer){
     if(answer){
         Firebird.attach(options, async function(err, db){
-            if(err) throw err
-        
+            if(err){
+                console.log(chalk.red('\n Error! Could not connect to database!'))             
+                //process.exit(1)
+                throw err
+            }           
+
             console.log(chalk.yellow(' Connected to DB successfully! \n'))
         
             const R1000Ids = await findR1000Id(db)
-          
+            
             const ids = await extractIdsFromArrayOfObjects(R1000Ids)
             
             const ppks =  await findPpksAndIds(db, ids)            
 
             const res = await insertBlockModDir(db, ppks)
-                       
+                        
             db.detach()
 
             await sleep(5000)
             
-            console.log(chalk.yellow('\n Operation completed successfully!')) // will be seen if program was run using .bat file
+            console.log(chalk.yellow('\n Operation completed successfully!')) // will be seen if program was run using .bat file                
         })
     }
     else {
@@ -60,7 +64,10 @@ function findR1000Id(db){
         const findR1000IdQuery = `SELECT id FROM objects WHERE class_name = 'ATU_DNBR1000';`    
     
         db.query(findR1000IdQuery, function(err, result){
-            if(err) throw err
+            if(err) {
+                console.log(chalk.red('\n Error! Could not find R1000 Ids in database!'))  
+                throw err
+            }
               
             resolve(result)            
         })
@@ -74,7 +81,10 @@ function findPpksAndIds(db, R1000Ids){
         const findPpksQuery = `SELECT id, oid, name FROM objects WHERE parent_id IN (${R1000Ids}) AND oid > 0;`
     
         db.query(findPpksQuery, function(err, result){
-            if(err) throw err
+            if(err) {
+                console.log(chalk.red('\n Error! Could not find ppks Ids in database!'))  
+                throw err
+            }
 
             resolve(result)
         })
@@ -98,7 +108,10 @@ function insertBlockModDir(db, ids){
                 WHERE child_id = ${ids[i].ID};`
     
             db.query(query, async function(err, result){
-                if(err) throw err           
+                if(err) {
+                    console.log(chalk.red(`\n Error! Could not insert data for ppk ${ids[i].OID}, "${ids[i].NAME}" in database!`))  
+                    throw err
+                }           
     
                 console.log(chalk.green(` Inserted for ppk ${ids[i].OID}, ${ids[i].NAME}`))
             })
